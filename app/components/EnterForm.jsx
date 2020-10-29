14b97682c77ee321af05a94e924df93f2c971a0b
Tirkirill <sherbakirill@mail.ru>
2020-10-29 16:52:22 +0300

    Стили формы

--- app/components/EnterForm.jsx
+++ app/components/EnterForm.jsx
@@ -9,7 +9,11 @@ class EnterForm extends React.Component {
         return(
             <div class='center-container'>
                 <div class='form-container'>
-
+                    <span class='form-title'>Вход</span>
+                    <form>
+                        <input type='email' placeholder='Введите email' class='email'></input>
+                        <input placeholder='Введите пароль' class='password'></input>
+                    </form>
                 </div>
             </div>
         )
