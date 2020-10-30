dc3790730ab969edd8e7c1dc1790f4e107446a1b
Tirkirill <sherbakirill@mail.ru>
2020-10-31 01:22:19 +0300

    Main

--- app/components/Main/Main.jsx
+++ app/components/Main/Main.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+let posts = [
+    {
+        title: "Привет!",
+        text: "ВОТО-Че придумал",
+        id:"1"
+    },
+    {
+        title: "Приветули!",
+        text: "Ниче не придумал...",
+        id:"2"
+    }
+]
 
 class Main extends React.Component {
     constructor(props) {
@@ -9,7 +21,18 @@ class Main extends React.Component {
     render() {
         if (!this.props.user) return null;
         return(
-            <div>{this.props.user.email}</div>
+            <div>
+                <div className='navbar'>
+                    <div className='addButton'></div>
+                    <span className='useremail'>{this.props.user.email}</span>
+                </div>
+                <ul className='ideaList'>
+                    {posts.map((post)=> 
+                    <li draggable className='idea' key={post.id}>
+                        <div className='idea-title'>{post.title.length>10? post.title.slice(0, 10)+"...":post.title}</div>
+                    </li>)}
+                </ul>
+            </div>
         )
     }
 } 
