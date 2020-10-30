35f7c0b322e96c336c1ab4d056e77ada1d1ffcb8
Tirkirill <sherbakirill@mail.ru>
2020-10-31 02:15:10 +0300

    Записи перетаскиваются

--- app/components/Main/Main.jsx
+++ app/components/Main/Main.jsx
@@ -1,21 +1,66 @@
 import React from "react";
+import $ from "jquery";
+import 'jquery-ui-dist/jquery-ui';
 
 let posts = [
     {
         title: "Привет!",
         text: "ВОТО-Че придумал",
-        id:"1"
+        id:"1",
+        X:50,
+        Y:70
     },
     {
         title: "Приветули!",
         text: "Ниче не придумал...",
-        id:"2"
+        id:"2",
+        X:200,
+        Y:100
     }
 ]
 
 class Main extends React.Component {
     constructor(props) {
         super(props);
+        this.addButton_click = this.addButton_click.bind(this);
+        let ideas = JSON.parse(sessionStorage.getItem('ideas'));
+        console.log(ideas);
+        if (ideas) this.state = {ideas:ideas};
+        else {
+            this.state = {ideas: posts};
+        }
+        this.stopDrag = this.stopDrag.bind(this);
+        
+    }
+
+    componentDidUpdate() {
+        $('.ideaList li').draggable(
+            {stop: this.stopDrag
+        });
+    }
+
+    addButton_click() {
+        this.setState({
+             ideas: this.state.ideas.concat({title: 'new', id: new Date().getTime()})
+        })
+    }
+
+    stopDrag(event, ui) {
+        let newIdeas = this.state.ideas.slice();        
+        let id = event.target.id;
+        newIdeas[id].X = ui.position.left;
+        newIdeas[id].Y = ui.position.top;
+        this.setState({ideas: newIdeas})
+    }
+
+    componentDidMount() {
+        $('.ideaList li').draggable(
+        {stop: this.stopDrag
+        });
+    }
+
+    componentDidUpdate() {
+        sessionStorage.setItem('ideas', JSON.stringify(this.state.ideas));
     }
 
     render() {
@@ -23,13 +68,13 @@ class Main extends React.Component {
         return(
             <div>
                 <div className='navbar'>
-                    <div className='addButton'></div>
+                    <div className='addButton' onClick={this.addButton_click}></div>
                     <span className='useremail'>{this.props.user.email}</span>
                 </div>
                 <ul className='ideaList'>
-                    {posts.map((post)=> 
-                    <li draggable className='idea' key={post.id}>
-                        <div className='idea-title'>{post.title.length>10? post.title.slice(0, 10)+"...":post.title}</div>
+                    {this.state.ideas.map((post, i)=> 
+                    <li style={{top: post.Y, left: post.X}} className='idea' id={i} key={post.id}>
+                        <div className='idea-title' >{post.title.length>10? post.title.slice(0, 10)+"...":post.title}</div>
                     </li>)}
                 </ul>
             </div>
